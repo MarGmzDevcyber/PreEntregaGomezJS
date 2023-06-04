8a9b2af24361abd886be8102dff102eeb5dcd396
Maria Gomez <msaam.gmz00@gmail.com>
2023-06-04 20:50:56 -0300

    cambios preentrega 3 js

--- main.js
+++ main.js
@@ -1,35 +1,41 @@
-function sim() {
-        //entrada con prompt
-        let costoPlacaVideo = parseInt(prompt("Ingrese el costo de la placa de video:"));
-        let costoPlacaMadre = parseInt(prompt("Ingrese el costo de la Placa Madre:"));
-        let costoRAM = parseInt(prompt("Ingrese el costo de la memoria RAM:"));
-        let costoDiscoDuro = parseInt(prompt("Ingrese el costo del disco duro:"));
-        let costoGabinete = parseInt(prompt("Ingrese el costo del gabinete:"));
-    
-        let confirmar = prompt(`¿Desea confirmar los costos de la Placa de Video ${costoPlacaVideo}, Placa Madre ${costoPlacaMadre}, RAM ${costoRAM}, disco duro ${costoDiscoDuro} y gabinete ${costoGabinete}? si || no`);
-    
-        while (confirmar === "no") {
-        costoPlacaVideo = parseInt(prompt("Ingrese el costo de la placa de video:"));
-        costoPlacaMadre = parseInt(prompt("Ingrese el costo de la Placa Madre:"));
-        costoRAM = parseInt(prompt("Ingrese el costo de la memoria RAM:"));
-        costoDiscoDuro = parseInt(prompt("Ingrese el costo del disco duro:"));
-        costoGabinete = parseInt(prompt("Ingrese el costo del gabinete:"));
-        confirmar = prompt(`¿Desea confirmar los costos de la Placa de Video ${costoPlacaVideo}, Placa Madre ${costoPlacaMadre}, RAM ${costoRAM}, disco duro ${costoDiscoDuro} y gabinete ${costoGabinete}? si || no`);
-        }
-    
-        //funcion que calcula el costo total
-        function calcularCostoTotal(costoPlacaVideo, costoPlacaMadre, costoRAM, costoDiscoDuro, costoGabinete) {
+document.getElementById("sim").addEventListener("click", function() {
+    const costoPlacaVideo = parseInt(document.getElementById("costoPlacaVideo").value);
+    const costoPlacaMadre = parseInt(document.getElementById("costoPlacaMadre").value);
+    const costoRAM = parseInt(document.getElementById("costoRAM").value);
+    const costoDiscoDuro = parseInt(document.getElementById("costoDiscoDuro").value);
+    const costoGabinete = parseInt(document.getElementById("costoGabinete").value);
+
+    const costoTotal = calcularCostoTotal(costoPlacaVideo, costoPlacaMadre, costoRAM, costoDiscoDuro, costoGabinete);
+
+    const resultadoHTML = document.getElementById("resultado");
+    resultadoHTML.innerHTML = `El costo total de la PC es de ${costoTotal} Pesos.`;
+
+    // Almacenar datos en el LocalStorage como JSON
+    const datos = {
+        costoPlacaVideo,
+        costoPlacaMadre,
+        costoRAM,
+        costoDiscoDuro,
+        costoGabinete
+    };
+    localStorage.setItem("datos", JSON.stringify(datos));
+});
+
+  // Función para calcular  costo total
+    function calcularCostoTotal(costoPlacaVideo, costoPlacaMadre, costoRAM, costoDiscoDuro, costoGabinete) {
         const costoTotal = costoPlacaVideo + costoPlacaMadre + costoRAM + costoDiscoDuro + costoGabinete;
-        return costoTotal;
-        }
-    
-        //utilizo un llamado a la funcion para calcular el total
-        const costoTotal = calcularCostoTotal(costoPlacaVideo, costoPlacaMadre, costoRAM, costoDiscoDuro, costoGabinete);
-    
-        // el resultado sale en un alert
-        alert(`El costo total de la PC es de ${costoTotal} Pesos.`);
+            return costoTotal;
     }
-    
-        // llamada función principal
-    sim();
-    
+
+    // Carga de datos almacenados en el LocalStorage al cargar pagina
+
+    window.addEventListener("load", function() {
+
+        const datos = localStorage.getItem("datos");
+            if (datos) {
+                const { costoPlacaVideo, costoPlacaMadre, costoRAM, costoDiscoDuro, costoGabinete } = JSON.parse(datos);
+                document.getElementById("costoPlacaVideo").value = costoPlacaVideo;
+                document.getElementById("costoPlacaMadre").value = costoPlacaMadre;
+                document.getElementById("costoRAM").value = costoRAM;
+                document.getElementById("costoDiscoDuro").value = costoDiscoDuro;
+                document.getElementById("costoGabinete").value}})
